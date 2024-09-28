7b81093070a82a5e70ed0bb52aea1a5ee6f5c5ae
Moises Molina <mo18022@ues.edu.sv>
2024-09-28 13:23:09 -0600

    cambio en cors3

--- models/sockets.js
+++ models/sockets.js
@@ -10,7 +10,7 @@ class Sockets {
 
     socketEvent() {
         this.io.on('connection', (socket) => {
-
+            console.log('Se ha conectado');
             socket.on('mensaje-to-server', (msg) => {
                 console.log('el cliente dice: ' + JSON.stringify(msg));
                 this.io.emit('mensaje-from-server', msg);
